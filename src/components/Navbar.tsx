4ff42b23f0347cde3191bfac6b80dd107d796b31
maytees <mateespublicprofile@gmail.com>
2024-04-21 14:20:04 -0400

    fix: sm breakpoint hamburger menu fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,48 +4,57 @@ import Link from 'next/link';
 import React from 'react';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 import { Button } from './ui/button';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 const ICON_WIDTH = 64;
 
 const Navbar = () => {
     const currentPath = usePathname();
 
+    const router = useRouter();
     return (
-        <>
-            <header className="pb-10 z-50 top-0 flex h-16 items-center gap-4 md:px-6 mt-10 pt-10">
-                <nav className="hidden container md:flex flex-row md:items-center md:justify-between gap-24 md:px-14 lg:px-32 w-full ">
 
-                    <div className='flex flex-row gap-16 items-center'>
-                        <div className='flex-shrink-0'>
-                            <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="/">
-                                <Image src="/logo.svg" className='flex-shrink-0' alt="Guideway" width={ICON_WIDTH} height={ICON_WIDTH} />
-                                <span className="sr-only">Guideway</span>
-                            </Link>
-                        </div>
-                        <div className='flex gap-5 lg:gap-10 *:text-center *:font-semibold'>
-                            <Link className="text-foreground transition-colors hover:text-foreground" href="/features">
-                                Features
-                            </Link>
-                            <Link className={`text-black hover:text-muted-foreground ${currentPath === '/pricing' ? 'text-foreground' : ''}`} href="/pricing">
-                                Pricing
-                            </Link>
-                            <Link className={`text-black hover:text-muted-foreground ${currentPath === '/support' ? 'text-foreground' : ''}`} href="/support">
-                                Support
-                            </Link>
-                            <Link className={`text-black hover:text-muted-foreground ${currentPath === '/faq' ? 'text-foreground' : ''}`} href="/faq">
-                                FAQ
-                            </Link >
-                        </div>
+        <div className="pb-10 px-10 z-50 top-0 flex h-16 items-center gap-4 md:px-6 mt-10 pt-10">
+            <nav className="hidden container md:flex flex-row md:items-center md:justify-between gap-24 md:px-14 lg:px-32">
+                <div className='flex flex-row gap-16 items-center'>
+                    <div className='flex-shrink-0'>
+                        <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="/">
+                            <Image src="/logo.svg" className='flex-shrink-0' alt="Guideway" width={ICON_WIDTH} height={ICON_WIDTH} />
+                            <span className="sr-only">Guideway</span>
+                        </Link>
                     </div>
+                    <div className='flex gap-5 lg:gap-10 *:text-center *:font-semibold'>
+                        <Link className="text-foreground transition-colors hover:text-foreground" href="/features">
+                            Features
+                        </Link>
+                        <Link className={`text-black hover:text-muted-foreground ${currentPath === '/pricing' ? 'text-foreground' : ''}`} href="/pricing">
+                            Pricing
+                        </Link>
+                        <Link className={`text-black hover:text-muted-foreground ${currentPath === '/support' ? 'text-foreground' : ''}`} href="/support">
+                            Support
+                        </Link>
+                        <Link className={`text-black hover:text-muted-foreground ${currentPath === '/faq' ? 'text-foreground' : ''}`} href="/faq">
+                            FAQ
+                        </Link >
+                    </div>
+                </div>
 
 
-                    <a href="/auth/register">
-                        <Button className="shrink-0 font-semibold" size="lg" variant="default">
-                            Get started
-                        </Button>
-                    </a>
-                </nav >
+                <a href="/auth/register">
+                    <Button className="shrink-0 font-semibold" size="lg" variant="default">
+                        Get started
+                    </Button>
+                </a>
+            </nav >
+            <div className="flex w-full items-center flex-row justify-between md:hidden">
+                <Image
+                    src="/logo.svg"
+                    alt="Guideway"
+                    width={ICON_WIDTH}
+                    height={ICON_WIDTH}
+                    className="md:hidden hover:cursor-pointer"
+                    onClick={() => router.push("/")}
+                />
                 <Sheet>
                     <SheetTrigger asChild>
                         <Button className="shrink-0 md:hidden" size="icon" variant="outline">
@@ -53,13 +62,12 @@ const Navbar = () => {
                             <span className="sr-only">Toggle navigation menu</span>
                         </Button>
                     </SheetTrigger>
-                    <SheetContent side="left">
+                    <SheetContent side="right">
                         <nav className="grid gap-6 text-lg font-medium">
                             <Link className="flex items-center gap-2 text-lg font-semibold" href="/">
                                 <Image src="/logo.svg" alt="Guideway" width={ICON_WIDTH} height={ICON_WIDTH} />
                                 <span className="sr-only">Guideway</span>
                             </Link>
-
                             <Link className="text-foreground transition-colors hover:text-foreground" href="/features">
                                 Features
                             </Link>
@@ -75,8 +83,8 @@ const Navbar = () => {
                         </nav>
                     </SheetContent>
                 </Sheet>
-            </header >
-        </>
+            </div>
+        </div>
     );
 };
 
