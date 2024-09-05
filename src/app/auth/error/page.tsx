1cb7e6999ffe5073e68e28e1aa62c1c0a65b516b
maytees <mateespublicprofile@gmail.com>
2024-09-05 14:10:37 -0400

    feat: uncomment error page

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,45 +1,53 @@
-// "use client";
+"use client";
 
-// import { useRouter, useSearchParams } from 'next/navigation';
-// import React, { useEffect, useState } from 'react'
-// import FormCard from '~/components/forms/FormCard';
-// import { ErrorComponent } from '~/components/forms/FormInfo';
-// import { Button } from '~/components/ui/button';
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import FormCard from "~/components/forms/FormCard";
+import { Button } from "~/components/ui/button";
 
-// // Returns JSX page of error
-// const getErrorPage = (code: string | null): React.ReactNode | null => {
-//     switch (code) {
-//         case "409":
-//             return null
-//         case "410":
-//             return VerifyEmail()
-//         default:
-//             return null
-//     }
-// }
+// Returns JSX page of error
+const getErrorPage = (code: string | null): React.ReactNode | null => {
+  switch (code) {
+    case "409":
+      return null;
+    case "410":
+      return VerifyEmail();
+    default:
+      return null;
+  }
+};
 
-// const VerifyEmail = () => {
-//     return (
-//         <div>
-//             Email needs verifying
-//         </div>
-//     )
-// }
+const VerifyEmail = () => {
+  return <div>Email needs verifying</div>;
+};
 
-// const Error = () => {
-//     const searchParams = useSearchParams();
-//     const errorCode = searchParams.get("code");
+const Error = () => {
+  const searchParams = useSearchParams();
+  const errorCode = searchParams.get("code");
 
-//     return getErrorPage(errorCode);
-//     // <FormCard title={"Oops.."} description={"Something went wrong.."}>
-//     //     <div className="mt-2 space-y-2">
-//     //         <ErrorComponent message={error} />
-//     //         <Button type="submit" className='w-full' onClick={() => {
-//     //             router.push("/auth/register")
-//     //         }}>Back to register</Button>
-//     //     </div>
-//     // </FormCard >
+  const router = useRouter();
 
-// }
+  const error = getErrorPage(errorCode);
 
-// export default Error
+  if (error) {
+    return error;
+  }
+
+  return (
+    <FormCard title={"Oops.."} description={"Something went wrong.."}>
+      <div className="mt-2 space-y-2">
+        <Button
+          type="submit"
+          className="w-full"
+          onClick={() => {
+            router.push("/auth/register");
+          }}
+        >
+          Back to register
+        </Button>
+      </div>
+    </FormCard>
+  );
+};
+
+export default Error;
